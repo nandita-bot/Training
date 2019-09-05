9ff2eeb9058fc3d74fc06aeaa094f28ce4a5892b
Nandita Singh <nanditas@successive.com>
2019-09-05 16:08:46 +0530

    Java_Task

---
+++ Java_Task_Day3/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            int r=3, c=6,i,j;
+
+            for(i = 1; i <= r; i++)
+            {
+                for(j = 1; j <= c; j++)
+                {
+                    System.out.print(" * ");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
